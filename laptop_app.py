e06c4b6d618d9dd66cfa4462a60a4c94c554d4d7
Lokeswar Gahir <111264240+lokeswar-gahir@users.noreply.github.com>
2023-06-18 11:14:27 +0530

    Screen size (in inches)

--- laptop_app.py
+++ laptop_app.py
@@ -28,7 +28,7 @@ touchscreen = st.selectbox("Touchscreen",["No","Yes"])
 ips = st.selectbox("IPS",["No","Yes"])
 
 # Screen size
-screen_size = st.number_input("Screen Size")
+screen_size = st.number_input("Screen Size(in inches)")
 
 # Screen resolution
 resolution = st.selectbox("Screen Resolution",["1920x1080","1366x768","1600x900","3840x2160","3200x1800","2880x1800","2560x1600","2560x1440","2304x1440"])
